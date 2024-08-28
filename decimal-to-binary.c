c8822ab92d6a27811cbbd13eb826e642520d8ff6
Duckman <David.Ca3545@gmail.com>
2024-08-27 20:22:04 -0700

    Added executable directory to run tests for files

--- decimal-to-binary.c
+++ decimal-to-binary.c
@@ -3,17 +3,17 @@
 
 int main(void)
 {
-  int dividend, remainder, quotient, itr = 0;
+  int dividend, itr = 0;
   printf("This program converts decimal to binary: ");
   scanf("%d", &dividend);
   int rem[SIZE];
   do
   {
     rem[itr] = dividend % 2; 
-    quotient = dividend / 2;
+    dividend = dividend / 2;
     itr++;   
   }
-  while(quotient != 0);
+  while(dividend != 0);
   for(int i = itr-1; i >= 0; i--)
   {
     printf("%d", rem[i]);
